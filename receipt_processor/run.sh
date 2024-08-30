e38474fa3f0bfd41934b37609cc1154a39c05f98
Durbek-Gafur <durbek.gafurov@gmail.com>
2024-08-30 17:44:47 -0500

    mvp

--- receipt_processor/run.sh
+++ receipt_processor/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Stop and remove any container using port 8081
-docker ps -q --filter "publish=8081" | xargs -r docker stop | xargs -r docker rm
+if [ $(docker ps -q -a -f name=receipt_processor_durbek) ]; then
+    docker stop receipt_processor_durbek
+    docker rm receipt_processor_durbek
+fi
 
 # Build the Docker image
-docker build -t receipt_processor .
+docker build -t receipt_processor_durbek .
 
 # Run the Docker container, mapping port 8081 of the container to port 8081 of the host machine
-docker run -d -p 8081:8081 --name receipt_processor receipt_processor
+docker run -d -p 8081:8081 --name receipt_processor_durbek receipt_processor_durbek
 
 # Print a message to indicate where the server can be accessed
 echo "Server is running at http://localhost:8081"
