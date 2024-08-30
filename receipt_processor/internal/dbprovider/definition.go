9c9abfee208b29cdd2096c5cac75b75882f55c88
Durbek-Gafur <durbek.gafurov@gmail.com>
2024-08-30 18:03:27 -0500

    file restructure

--- receipt_processor/internal/dbprovider/definition.go
+++
@@ -1,7 +0,0 @@
-package db
-
-//go:generate mockgen -source=definition.go -destination=./mocks/mock_db.go -package=mock_db
-type DB interface {
-	SavePoints(id string, points int) error
-	GetPointByReceiptID(id string) (int, error)
-}
